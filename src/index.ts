b684d3a168cc4e81ec6fb4dc47472e2bd8878cd4
manvendra pratap singh <manvendra7129@gmail.com>
2024-04-27 15:44:05 +0530

    todays learning

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-const text="my first string"
+// let a =25;
+// type FuncType =(n:number,m:number,l?:number)=>number;
+// const func:FuncType=(n,m,l=69)=>{
+//   return n*m*l;
+// }
+// console.log(func(25,23))
+
+//rest operator
+// type FuncType =(...m:number[])=>number[];
+// const func:FuncType=(...m:number[])=>{
+//   return m;
+// }
+// console.log(func(25,23,55,44,66,77))
+
+// function normalFunction(n:number):number{
+//                                            return n;
+//                                         }
+//normalfunction with type
+  
+// function with object
+
+const getData =(product:{name:string,stock:number,price:number,photo:string})=>{
+   console.log(product);
+}
+getData({name:"string",stock:123,price:456,photo:"string"})
